42f0b1bc80635e83491d5fb5fda526d18b2f3d55
Jheison J. Mayta C. <8401842+jmayta-caceres@users.noreply.github.com>
2024-04-14 13:48:21 -0500

    remove automapper temporaly

--- src/uom/MW.SUNQU.UOM.Application/DependencyInjection.cs
+++ src/uom/MW.SUNQU.UOM.Application/DependencyInjection.cs
@@ -4,11 +4,10 @@ namespace MW.SUNQU.UOM.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         var applicationAssembly = typeof(DependencyInjection).Assembly;
         services.AddMediatR(config => config.RegisterServicesFromAssembly(applicationAssembly));
-        services.AddAutoMapper(applicationAssembly);
 
         return services;
     }
